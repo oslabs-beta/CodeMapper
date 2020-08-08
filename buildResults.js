706ad30963bb6452726a26c31d1d2c6910222416
Beckett Hanan <beckett.hanan@gmail.com>
2020-08-08 16:17:43 -0700

    adding empty files that are placeholders for functions we'll use to generate results for the end user

---
+++ buildResults.js
@@ -0,0 +1,1 @@
+// this should build the results page for the end user
---
+++ createStructureResult.js
@@ -0,0 +1,7 @@
+// this file will generate the foamtree visualization of the file/folder structure
+
+const generate = () => {
+  // we should return the final result here, or maybe an object with a visual and non-visual option
+};
+
+module.exports = { generate };
