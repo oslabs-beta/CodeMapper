67bc1c3e0bc33ed54afb70fe78d0890b06436924
Nadya Minkova <nadya@broko.bg>
2020-08-21 05:43:55 +0300

    intermediate stage - before Lodash

--- src/flow.js
+++ src/flow.js
@@ -13,30 +13,39 @@ async function flow(root, include, exclude) {
   const fileTree = await generateTree(root, include, exclude);
 
   // these two could be concurrent
-    // call createStructureResult with the file tree passed in
-    // to generate the file/folder structure result
-    // createStructureResult(fileTree);
+  // call createStructureResult with the file tree passed in
+  // to generate the file/folder structure result
+  // createStructureResult(fileTree);
 
-    // call filter, passing in the file tree, to get an array of pointers to the JS file objects
-    // this will also pass all the js files to the parser
+  // call filter, passing in the file tree, to get an array of pointers to the JS file objects
+  // this will also pass all the js files to the parser
   try {
     if (fileTree !== undefined) {
       filterAndParse(fileTree);
+
+      fs.writeFileSync(
+        PATH.resolve(__dirname, '../data/fileTree.json'),
+        JSON.stringify(fileTree, null, 2)
+      );
       // create foamTree data for browser project tree data visualization
       writeFoamTreeData(fileTree);
 
-      fs.writeFileSync(PATH.resolve(__dirname, '../data/finalTree.json'), JSON.stringify(fileTree, null, 2));
-      console.log('\x1b[32m', 'All done! look in data/finalTree.json to see the current result.\x1b[37m');
+      console.log(
+        '\x1b[32m',
+        'All done! look in data/finalTree.json to see the current result.\x1b[37m'
+      );
     }
   } catch (err) {
-    console.error(`\n\x1b[31mError in flow.js with filterAndParse(fileTree): ${err.message}\x1b[37m`);
+    console.error(
+      `\n\x1b[31mError in flow.js with filterAndParse(fileTree): ${err.message}\x1b[37m`
+    );
   }
 
   // our original fileTree should now be modified to give us what we need for generating other results
   // so we'll pass it to our other results-generating functions
   // these can happen concurrently
-    // const dependencies = createDependencyResult(fileTree);
-    // const functionality = createFunctionalityResult(fileTree);
+  // const dependencies = createDependencyResult(fileTree);
+  // const functionality = createFunctionalityResult(fileTree);
 
   // put all the results into an array
   // const results = [structure, dependencies, functionality];
