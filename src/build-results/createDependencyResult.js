5328b53cc507d8289c05126e3e314dea815724ea
Beckett Hanan <beckett.hanan@gmail.com>
2020-08-22 13:20:19 -0700

    initial work on creating the dependency chart for the front-end

--- src/build-results/createDependencyResult.js
+++ src/build-results/createDependencyResult.js
@@ -1,1 +1,49 @@
-// this should create our dependencies section of the results for the end user
+document.addEventListener('DOMContentLoaded', () => {
+  // this should create our dependencies section of the results for the end user
+  const dependencyChart = Highcharts.chart('container', {
+
+    title: {
+      text: 'Highcharts Dependency Wheel',
+    },
+
+    accessibility: {
+      point: {
+        valueDescriptionFormat: 'From {point.from} to {point.to}: {point.weight}.'
+      },
+    },
+
+    series: [{
+      keys: ['from', 'to', 'imports'],
+      data: [
+        ['readline', 'getUserInput.js', 1],
+        ['path', 'getUserInput.js', 1],
+        ['generateFileTree.js', 'getUserInput.js', 1],
+        ['flow.js', 'getUserInput.js', 1],
+        ['@babel/parser', 'parser.js', 1],
+        ['@babel/core', 'parser.js', 1],
+        ['@babel/traverse', 'parser.js', 1],
+        ['@babel/generator', 'parser.js', 1],
+        ['fs', 'parser.js', 1],
+        ['transform.js', 'parser.js', 1],
+        ['@babel/generator', 'transform.js', 1],
+        ['parser.js', 'filterAndParse.js', 1],
+        ['fs', 'filewalker.js', 1],
+        ['path', 'filewalker.js', 1],
+      ],
+      type: 'dependencywheel',
+      name: 'Dependency wheel series',
+      dataLabels: {
+        color: '#333',
+        textPath: {
+          enabled: true,
+          attributes: {
+            dy: 5,
+          },
+        },
+        distance: 10,
+      },
+      size: '95%',
+    }],
+
+  });
+});
