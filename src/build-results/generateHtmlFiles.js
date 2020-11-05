891652d31fd2314bbd60597404ce9a9f822e3e0d
Beckett Hanan <beckett.hanan@gmail.com>
2020-11-04 20:21:23 -0800

    changing throw to console.log in generate html file function

--- src/build-results/generateHtmlFiles.js
+++ src/build-results/generateHtmlFiles.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 // this creates all the html files so the end user can see the results of their codebase analysis
 const generateHTMLfiles = async (pathToSource, pathToDestination) => {
+  console.log('got into generateHTMLfiles');
   const files = await fs.readdir(pathToSource);
 
   files.forEach(async (file) => {
@@ -14,7 +15,7 @@ const generateHTMLfiles = async (pathToSource, pathToDestination) => {
         data,
         'utf8',
         (err) => {
-          if (err) throw err;
+          if (err) console.log('error is ', err);
         }
       );
     } catch (error) {
