1e004f75e5ee1c1febde398a9bb56e3d0dede041
Beckett Hanan <beckett.hanan@gmail.com>
2020-11-07 15:50:16 -0800

    fixing the correct thing this time

--- getRoot.js
+++ getRoot.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 
-const data = `const modulePath = '${path.resolve(process.argv[1], '..', '..', 'visualization')}';`;
+const data = `const modulePath = '${path.resolve(process.argv[1], '..', 'visualization')}';`;
 console.log('data is ', data);
-const writePath = path.resolve(process.argv[1], '..', '..', 'node-module-path.js');
+const writePath = path.resolve(process.argv[1], '..', 'node-module-path.js');
 console.log('writePath is ', writePath);
 
 try {
@@ -16,6 +16,6 @@ try {
   console.log('shoot', err);
 }
 
-console.log('our path is ', path.resolve(process.argv[1], '..', '..', 'visualization'));
+console.log('our path is ', path.resolve(process.argv[1], '..', 'visualization'));
 
-module.exports = path.resolve(process.argv[1], '..', '..', 'visualization');
+module.exports = path.resolve(process.argv[1], '..', 'visualization');
