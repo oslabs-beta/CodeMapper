8ee59aa18634ec4823a0bd333084a34e2185b878
Beckett Hanan <beckett.hanan@gmail.com>
2020-11-07 15:43:11 -0800

    updating getRoot to write the path to the visualization folder in the node module

--- getRoot.js
+++ getRoot.js
@@ -1,5 +1,21 @@
+const fs = require('fs');
 const path = require('path');
 
-console.log('initial path is process.argv[1], and then our path is ', path.resolve(process.argv[1], '..', '..', 'visualization'));
+const data = `const modulePath = '${path.resolve(process.argv[1], '..', '..', 'visualization')}';`;
+console.log('data is ', data);
+const writePath = path.resolve(process.argv[1], '..', 'node-module-path.js');
+console.log('writePath is ', writePath);
+
+try {
+  fs.writeFileSync(
+    path.resolve(writePath),
+    data,
+    'utf8',
+  );
+} catch (err) {
+  console.log('shoot', err);
+}
+
+console.log('our path is ', path.resolve(process.argv[1], '..', '..', 'visualization'));
 
 module.exports = path.resolve(process.argv[1], '..', '..', 'visualization');
